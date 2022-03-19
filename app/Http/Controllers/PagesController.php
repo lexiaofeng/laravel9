5dd71ea89f649251d7352f2b64fc0a740ccf12ff
lexiaofeng <1411582219@qq.com>
2022-03-19 23:17:54 +0800

    强制用户认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,19 +8,6 @@ class PagesController extends Controller
 {
     public function root()
     {
-//        try {
-//            $dbh = new \PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');
-//        }
-//        catch (\PDOException $e) {
-//            die("数据库连接失败" . $e->getMessage());
-//        }
-//        $sql="SELECT * FROM user ";
-//
-//        $rs=$dbh->query($sql);
-//        $row=$rs->fetch();
-//        echo '<pre>';var_dump($row);exit;
-
-//        echo '<pre>';var_dump($dbh);exit;
         return view('pages.root');
     }
 }
